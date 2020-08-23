85e0612b2a6a8858aca5a8515d8281323eac01e2
Lokesh <lokesh.badgujar@gmail.com>
2020-08-23 13:47:51 +0530

    some login form validations and styles added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,7 +13,7 @@ export class LoginComponent implements OnInit {
   passwordHideFlag: boolean = true;
   confirmPasswordHideFlag: boolean = true;
   isRegisteredMobile: boolean = false;
-  step1: boolean = true;
+  step1: boolean;
 
   loginForm: FormGroup;
 
@@ -21,32 +21,44 @@ export class LoginComponent implements OnInit {
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<LoginComponent>,) {
       this.initForm();
-    }
+  }
+
+  get signupForm() {
+    return this.loginForm.get('signUpForm');
+  }
 
   ngOnInit() {
   }
 
   initForm() {
     this.loginForm = this.fb.group({
-      mobile: ['', Validators.required],
+      mobile: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
       otp: ['', Validators.required],
       signUpForm: this.fb.group({
         fName: ['', Validators.required],
         lName: ['', Validators.required],
-        email: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
         password: ['', Validators.required],
         confirmPassword: ['', Validators.required]
       })
     });
-    this.loginForm.get('signUpForm').disable();
+    this.setStep1();
   }
 
   signInClickHandler() {
     this.step1 = false;
     this.loginForm.get('mobile').disable();
+    this.loginForm.get('otp').enable();
     if (!this.isRegisteredMobile) {
       this.loginForm.get('signUpForm').enable();
     }
+  }
+
+  setStep1() {
+    this.loginForm.get('mobile').enable();
+    this.loginForm.get('signUpForm').disable();
+    this.loginForm.get('otp').disable();
+    this.step1 = true;
   }
 
   verify() {
